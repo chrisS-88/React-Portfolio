16a6f82d5fcbde784f47fd5e4ea225d02500b111
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-03-02 21:31:40 +0000

    nav structure done, started home page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,73 @@
 import React from "react";
 
+const styles = {
+  sidebar: {
+    backgroundColor: "rgb(28, 28, 28)",
+    color: "white",
+    backdropFilter: "blur(10px)",
+  },
+};
+
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-md bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand portfolio-title" href="#">
-          Chris Smart
+    <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+      <div className="container">
+        <a className="navbar-brand fs-3" href="#">
+          Portfolio.
         </a>
+        <button
+          className="navbar-toggler shadow-none border-0"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        <div class="navbar-nav">
-          <a class="nav-link" href="#">
-            About
-          </a>
-          <a class="nav-link" href="#">
-            Skills
-          </a>
-          <a class="nav-link" href="#">
-            Projects
-          </a>
-          <a class="nav-link" href="#">
-            Contact
-          </a>
+        <div
+          className="sidebar offcanvas offcanvas-start"
+          tabindex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          style={styles.sidebar}
+        >
+          <div className="offcanvas-header text-white border-bottom">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              Portfolio
+            </h5>
+            <button
+              type="button"
+              className="btn-close btn-close-white shadow-none"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
